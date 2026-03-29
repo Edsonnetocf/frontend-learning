1633ede15e47acd48f03b388f06f9d995fe0d7e2
Edson Neto <edsonnetocf@gmail.com>
2026-03-29 19:15:24 -0300

    feat(js): add guess comparison and result feedback

--- projetos-integrados/logica-js-projeto_inicial/app.js
+++ projetos-integrados/logica-js-projeto_inicial/app.js
@@ -1,3 +1,11 @@
 alert("Bem vindo ao jogo do número secreto");
 let numeroSecreto = 29;
-prompt("Escolha um numero de entre 1 e 1000");
+console.log(numeroSecreto); //ver o numero secreto no log
+let tentativa = prompt("Escolha um numero de entre 1 e 1000");
+
+if(numeroSecreto == tentativa) {
+    alert("Você acertou o número secreto (29)");
+}
+else {
+    alert("Você errou");
+}
