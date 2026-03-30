ce9688fba42bcc6c58b440be952a0cd24bb5d88c
Edson Neto <edsonnetocf@gmail.com>
2026-03-29 21:05:00 -0300

    feat(js): allow multiple attempts with while loop

--- projetos-integrados/logica-js-projeto_inicial/app.js
+++ projetos-integrados/logica-js-projeto_inicial/app.js
@@ -1,16 +1,19 @@
 alert("Bem vindo ao jogo do número secreto");
 let numeroSecreto = 29;
 console.log(numeroSecreto); //ver o numero secreto no log
-let tentativa = prompt("Escolha um numero de entre 1 e 1000");
+let numTentativas = 1;
 
-if(tentativa == numeroSecreto) {
-    alert(`Você acertou o número secreto ${numeroSecreto}`);
-}
-else {
-   if(tentativa < numeroSecreto){
-        alert(`O número secreto é maior que ${tentativa}`);
-   }
-   else{
-        alert(`O número secreto é menor que ${tentativa}`);
-   }
+while(tentativa != numeroSecreto){
+    tentativa = prompt("Escolha um numero de entre 1 e 1000");
+    if(tentativa == numeroSecreto) {
+        alert(`Você acertou o número secreto (${numeroSecreto})`);
+    }
+    else {
+        if(tentativa < numeroSecreto){
+            alert(`O número secreto é maior que ${tentativa}`)
+        }
+        else{
+            alert(`O número secreto é menor que ${tentativa}`);
+        }
+    }
 }
