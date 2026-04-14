554c43a746f6c7ee62a35b9b18391526106547b0
Edson Neto <edsonnetocf@gmail.com>
2026-04-14 19:30:47 -0300

    refactor(js): extract random number generation into function

--- projetos-integrados/numero-secreto-dom/app.js
+++ projetos-integrados/numero-secreto-dom/app.js
@@ -7,6 +7,12 @@ function verificarChute(){
     console.log ('O botão foi clicado');
 }
 
+function gerarNumAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
+let numeroSecreto = gerarNumAleatorio();
+
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
