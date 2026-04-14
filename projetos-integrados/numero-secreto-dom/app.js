e3e07df2ea9cea8d452e7370c6e54661523740cc
Edson Neto <edsonnetocf@gmail.com>
2026-04-14 19:22:26 -0300

    refactor(js): extract reusable function to update DOM text

--- projetos-integrados/numero-secreto-dom/app.js
+++ projetos-integrados/numero-secreto-dom/app.js
@@ -1,10 +1,13 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
 function verificarChute(){
     console.log ('O botão foi clicado');
 }
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
 
