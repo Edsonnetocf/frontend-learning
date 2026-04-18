01401f39b7846475bf6a19f3ab2aa911f3c79114
Edson Neto <edsonnetocf@gmail.com>
2026-04-18 19:55:14 -0300

    feat(js): add array manipulation example with basic operations

---
+++ javascript/exercicios-pequenos/pratica-de-lista.js
@@ -0,0 +1,17 @@
+//criar
+let linguagensDeProgramacao = ['C', 'C++', 'Python', 'Java'];
+console.log(linguagensDeProgramacao);
+
+//adicionar
+linguagensDeProgramacao.push('GoLang');
+console.log(linguagensDeProgramacao);
+
+//remover
+linguagensDeProgramacao.pop();
+console.log(linguagensDeProgramacao);
+
+//mostrar primeiro elemento
+console.log(`primeiro elemento da lista: ${linguagensDeProgramacao[0]}`);
+
+//mostrar o ultimo elemento
+console.log(`ultimo elemento da lista: ${linguagensDeProgramacao[linguagensDeProgramacao.length -1]}`);
