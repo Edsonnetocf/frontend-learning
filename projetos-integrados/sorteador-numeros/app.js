e3580561d46b2cdbd5f3bb768e6047146998c085
Edson Neto <edsonnetocf@gmail.com>
2026-04-23 20:23:35 -0300

    feat(js): display drawn numbers in UI

--- projetos-integrados/sorteador-numeros/app.js
+++ projetos-integrados/sorteador-numeros/app.js
@@ -1,3 +1,12 @@
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;   //gera um numero aleatorio incluindo o max
+}
+
+function exibirTextoNaTela(id, texto) {
+    let campo = document.getElementById(id);
+    campo.innerHTML = texto;
+}
+
 function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);   //linhas 23, 28, 33 do html
     let de = parseInt(document.getElementById('de').value);                   //document.getElementById: devolve a tag html; .value:guarda na variavel quantidade o valor dentro do input
@@ -5,16 +14,18 @@ function sortear() {
     
     let sorteados = [];
     let numero;
-
+    
     for (let i = 0; i < quantidade; i++) {
-        numero = obterNumeroAleatorio(de, ate);
+        let numero = obterNumeroAleatorio(de, ate);
         sorteados.push(numero);
     }
     
     console.log(`Sorteados: ${sorteados}`);
+
+    exibirTextoNaTela('resultado', `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`);
 }
 
-function obterNumeroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;   //gera um numero aleatorio incluindo o max
-}
+
+
+
 
