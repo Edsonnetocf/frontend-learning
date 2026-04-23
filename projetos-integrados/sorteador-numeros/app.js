d3c491764a827e60f24bf62f2959dfeb300dd96e
Edson Neto <edsonnetocf@gmail.com>
2026-04-23 19:15:53 -0300

    feat(js): generate random numbers within specified range

--- projetos-integrados/sorteador-numeros/app.js
+++ projetos-integrados/sorteador-numeros/app.js
@@ -1,9 +1,20 @@
 function sortear() {
-    let quantidade = document.getElementById('quantidade').value;   //linhas 23, 28, 33 do html
-    let de = document.getElementById('de').value;                   //document.getElementById: devolve a tag html; .value:guarda na variavel quantidade o valor dentro do input
-    let ate = document.getElementById('ate').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);   //linhas 23, 28, 33 do html
+    let de = parseInt(document.getElementById('de').value);                   //document.getElementById: devolve a tag html; .value:guarda na variavel quantidade o valor dentro do input
+    let ate = parseInt(document.getElementById('ate').value);
     
-    console.log(`Quantidade: ${quantidade}`);
-    console.log(`De: ${de}`);
-    console.log(`Ate: ${ate}`);
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+    
+    console.log(`Sorteados: ${sorteados}`);
 }
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;   //gera um numero aleatorio incluindo o max
+}
+
