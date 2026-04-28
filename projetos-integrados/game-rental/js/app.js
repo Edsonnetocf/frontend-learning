010a5c80ff1a672050986b1c10be423f47c562e7
Edson Neto <edsonnetocf@gmail.com>
2026-04-27 21:54:11 -0300

    feat(js): toggle game rental status on button click

--- projetos-integrados/game-rental/js/app.js
+++ projetos-integrados/game-rental/js/app.js
@@ -1,5 +1,15 @@
+function alterarStatus(num) {
+    let jogo = document.getElementById(`game-${num}`);
+    let botao = jogo.querySelector('.dashboard__item__button');
+    let imagem = jogo.querySelector('.dashboard__item__img');
 
-let jogo = document.getElementById(`game-${num}`);
-let botao = jogo.querySelector('.dashboard__item__button');
-let imagem = jogo.querySelector('.dashboard__item__img');
-
+    if (botao.classList.contains("dashboard__item__button--return")) {
+            botao.classList.remove("dashboard__item__button--return");
+            imagem.classList.remove("dashboard__item__img--rented");
+            botao.textContent = "Alugar";
+        } else {
+            botao.classList.add("dashboard__item__button--return");
+            imagem.classList.add("dashboard__item__img--rented");
+            botao.textContent = "Devolver";
+    }
+}
